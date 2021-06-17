b1a8f6606b04f96a1e95bee48600505800e295e8
Ernesto Janebro <ernesto.janebro@servexternos.santander.pt>
2021-06-17 10:14:22 +0100

    Game state logic for win and lose

--- platformer.py
+++ platformer.py
@@ -10,6 +10,12 @@
 
 import pygame
 
+def drawText(string, x, y):
+  text = font.render(string, True, MUSTARD, DARK_GREY)
+  text_rect = text.get_rect()
+  text_rect.topleft = (x, y)
+  screen.blit(text, text_rect)
+
 # constant variables
 SCREEN_SIZE = (700,500)
 DARK_GREY = (50,50,50)
@@ -21,6 +27,9 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption('Janebro\'s Platform Game')
 clock = pygame.time.Clock()
 font = pygame.font.Font(pygame.font.get_default_font(), 24)
+
+# game_states = playing // win // lose
+game_state = 'playing'
 
 # player
 player_image = pygame.image.load('assets/vita/vita_00.png')
@@ -87,87 +96,95 @@ while running:
     if event.type == pygame.QUIT:
       running = False
   
-  new_player_x = player_x
-  new_player_y = player_y
+  if game_state == 'playing':
+    new_player_x = player_x
+    new_player_y = player_y
 
-  # player input
-  keys = pygame.key.get_pressed()
+    # player input
+    keys = pygame.key.get_pressed()
 
-  if keys[pygame.K_LEFT]:
-    new_player_x -= 2
-  if keys[pygame.K_RIGHT]:
-    new_player_x += 2
-  # if on the ground
-  if keys[pygame.K_SPACE] and player_on_ground:
-    player_speed = -5
-    # jump sound
-    jump_sfx = pygame.mixer.Sound('assets/sfx/jump.wav')
-    jump_sfx.set_volume(0.4)
-    jump_sfx.play()
+    if keys[pygame.K_LEFT]:
+      new_player_x -= 2
+    if keys[pygame.K_RIGHT]:
+      new_player_x += 2
+    # if on the ground
+    if keys[pygame.K_SPACE] and player_on_ground:
+      player_speed = -5
+      # jump sound
+      jump_sfx = pygame.mixer.Sound('assets/sfx/jump.wav')
+      jump_sfx.set_volume(0.4)
+      jump_sfx.play()
 
-  # ------
-  # UPDATE
-  # ------
+    # ------
+    # UPDATE
+    # ------
 
-  # horizontal movement
+    # horizontal movement
 
-  new_player_rect = pygame.Rect(new_player_x, player_y, player_width, player_height)
-  x_collision = False
+    new_player_rect = pygame.Rect(new_player_x, player_y, player_width, player_height)
+    x_collision = False
 
-  # ... check against every platform
-  for p in platforms:
-    if p.colliderect(new_player_rect):
-      x_collision = True
-      break
+    # ... check against every platform
+    for p in platforms:
+      if p.colliderect(new_player_rect):
+        x_collision = True
+        break
 
-  if x_collision == False:
-    player_x = new_player_x
+    if x_collision == False:
+      player_x = new_player_x
 
-  # vertical movement
+    # vertical movement
 
-  player_speed += player_acceleration
-  new_player_y += player_speed
+    player_speed += player_acceleration
+    new_player_y += player_speed
 
-  new_player_rect = pygame.Rect(player_x, new_player_y, player_width, player_height)
-  y_collision = False
-  player_on_ground = False
+    new_player_rect = pygame.Rect(player_x, new_player_y, player_width, player_height)
+    y_collision = False
+    player_on_ground = False
 
-  # ... check against every platform
-  for p in platforms:
-    if p.colliderect(new_player_rect):
-      y_collision = True
-      player_speed = 0
-      # if the platform is below the player
-      if p[1] > new_player_y:
-        # stick the player to the platform
-        player_y = p[1] - player_height
-        player_on_ground = True
-      break
+    # ... check against every platform
+    for p in platforms:
+      if p.colliderect(new_player_rect):
+        y_collision = True
+        player_speed = 0
+        # if the platform is below the player
+        if p[1] > new_player_y:
+          # stick the player to the platform
+          player_y = p[1] - player_height
+          player_on_ground = True
+        break
 
-  if y_collision == False:
-    player_y = new_player_y
+    if y_collision == False:
+      player_y = new_player_y
 
-  # see if any items have been collected
-  player_rect = pygame.Rect(player_x, player_y, player_width, player_height)
-  for c in collectables:
-    if c.colliderect(player_rect):
-      pick_sfx = pygame.mixer.Sound('assets/sfx/collect.wav')
-      pick_sfx.set_volume(0.5)
-      pick_sfx.play()
-      collectables.remove(c)
-      score += 1
+    # see if any items have been collected
+    player_rect = pygame.Rect(player_x, player_y, player_width, player_height)
+    for c in collectables:
+      if c.colliderect(player_rect):
+        pick_sfx = pygame.mixer.Sound('assets/sfx/collect.wav')
+        pick_sfx.set_volume(0.5)
+        pick_sfx.play()
+        collectables.remove(c)
+        score += 1
+        # changing game_state to win
+        if score >= 2:
+          game_state = 'win'
 
-  # see if any enemy hit
-  for e in enemies:
-    if e.colliderect(player_rect):
-      pick_sfx = pygame.mixer.Sound('assets/sfx/hurt.wav')
-      pick_sfx.set_volume(0.1)
-      pick_sfx.play()
-      lives -= 1
-      # reset player position
-      player_x = 300
-      player_y = 0
-      player_speed = 0
+
+    # see if any enemy hit
+    for e in enemies:
+      if e.colliderect(player_rect):
+        pick_sfx = pygame.mixer.Sound('assets/sfx/hurt.wav')
+        pick_sfx.set_volume(0.1)
+        pick_sfx.play()
+        lives -= 1
+        # reset player position
+        player_x = 300
+        player_y = 0
+        player_speed = 0
+        # changing game_state to lose
+        if lives <= 0:
+          game_state = 'lose'
 
   # -----
   # DRAW
@@ -195,14 +212,16 @@ while running:
 
   # score
   screen.blit(chicken_leg, (10, 10))
-  score_text = font.render(str(score), True, MUSTARD, DARK_GREY)
-  score_text_rect = score_text.get_rect()
-  score_text_rect.topleft = (45, 15)
-  screen.blit(score_text, score_text_rect)
+  drawText(str(score) , 45, 15)
 
   # lives
   for l in range(lives):
     screen.blit(lives_image, (600 + (l*30), 10))
+
+  if game_state == 'win':
+    drawText('YOU WIN!', 100, 100)
+  if game_state == 'lose':
+    drawText('YOU LOSE!', 100, 100)
 
   #present screen
   pygame.display.flip()
