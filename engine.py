73acf2fac0d8e42f2c529b6fbfd919a2a9f0fc8b
Ernesto Janebro <ernesto.janebro@servexternos.santander.pt>
2021-06-17 13:26:57 +0100

    Collectables animation

---
+++ engine.py
@@ -0,0 +1,17 @@
+import pygame
+
+class Animation():
+  def __init__(self, imageList):
+      self.imageList = imageList
+      self.imageIndex = 0
+      self.animationTimer = 0
+      self.animationSpeed = 25
+  def update(self):
+    self.animationTimer += 1
+    if self.animationTimer >= self.animationSpeed:
+      self.animationTimer = 0
+      self.imageIndex += 1
+      if self.imageIndex > len(self.imageList) - 1:
+        self.imageIndex = 0
+  def draw(self, screen, x, y):
+    screen.blit(self.imageList[self.imageIndex], (x, y))
